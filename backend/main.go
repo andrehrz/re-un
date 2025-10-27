ecffbf5b4d94a65622713a2d295ff44aa6ce409f
andre <hrz.andre@gmail.com>
2025-10-27 14:33:51 -0700

    backend server is running, added docker compose file to spin up db, and added .env files

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Backend is running")
+	fmt.Println("Starting backend")
+	if err := godotenv.Load(); err != nil {
+		log.Println("Could not load environment variables, no .env file found")
+	}
+
+	port := os.Getenv("PORT")
+	if port == ""{
+		port = "8080"
+	}
+
+	// this is my router
+	router := gin.Default()
+
+	// defining some simple GET endpoints
+	router.GET("/ping", func(c *gin.Context) {
+		// returns a JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+			"message": "server is running",
+		})
 	})
 
-	fmt.Println("Server listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	log.Printf("Server is running on port %s", port)
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal("Failed to start server")
+	}
 }
